69ede68466d835cf526407a24a5d9ac27d20ff97
k21060 <k21060kk@aitech.ac.jp>
2022-05-23 17:01:03 +0900

    json変更

--- test.py
+++ test.py
@@ -1,21 +1,52 @@
 import glob
-import csv
-from re import A
+# import csv
+# from re import A
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+# import numpy as np
+# import matplotlib.pyplot as plt
 import json
 
-df = pd.read_csv("./2021-12-29_000000.csv",names=("plece","all","small","big"))
+file = glob.glob("./2021-12-29_*.csv")
+# print(file)
+
+file = sorted(file)
+# print(file)
+
+df = []
+
+for b in file:
+    df.append(pd.read_csv(b,names=("plece","all","small","big")))
+
+# df = pd.read_csv("./2021-12-29_000000.csv",names=("plece","all","small","big"))
+
+# for b in df:
+#     print(b)
 
 
-a=df["small"]
-a_list = a.values.tolist()
+# directory = [0]*len(df)  
+
+
+# for i in range(len(df)):
+#     directory[i] = {}
+#     directory[i][f"csv_{i}"] = df[i]["small"].values.tolist()
 
 directory = {}
-directory["csv"] = a_list
+for i in range(len(df)):
+    directory[f"csv_{i}"] = df[i]["small"].values.tolist()
+
+
+
+# for c in df:
+#     directory = []
+#     directory["csv"] = df["small"].values.tolist()
+
+
 
 print(directory)
+
+# for i in range(len(df)):
+#     with open('test.json', 'w') as f:
+#         json.dump(directory[i], f, ensure_ascii=False)
 
 with open('test.json', 'w') as f:
     json.dump(directory, f, ensure_ascii=False)
